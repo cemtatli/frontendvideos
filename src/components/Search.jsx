106bc021cc85de84c6dc70216678a918996be3c7
cemtatli <cemtatli3535@gmail.com>
2023-04-06 16:10:18 +0300

    update search box

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
-import { FiSearch } from "react-icons/fi";
+import React from "react";
 
-export default function SearchBox({ videos, setFilteredVideos, filteredVideos }) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = (event) => {
-    const query = event.target.value.toLowerCase();
-    const filtered = videos.filter((video) => video.title.toLowerCase().includes(query));
-    setFilteredVideos(filtered);
-    setSearchTerm(query);
-  };
-
+export default function Search({ filteredVideos, setSearchTerm }) {
   return (
-    <div className="relative">
-      <div className="flex items-center overflow-hidden rounded-md bg-white shadow-sm">
-        <div className="flex items-center justify-center p-2">
-          <FiSearch className="text-gray-400" />
-        </div>
-        <input
-          type="text"
-          placeholder={`Listelenen ${filteredVideos.length} video arasından arama yapabilirsin. ✨`}
-          className="w-full rounded-md py-2 px-4 text-sm text-gray-900 placeholder-gray-400 focus:border-blue-300 focus:outline-none focus:ring-0"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </div>
-      {searchTerm.length > 0 && (
-        <ul className="absolute mt-1 max-h-48 w-full overflow-y-auto rounded-md bg-white shadow-md">
-          {filteredVideos.map((video) => (
-            <li key={video.id} className="cursor-pointer px-4 py-2 hover:bg-blue-100">
-              {video.title}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder={`Listelenen ${filteredVideos.length} video arasından arama yapabilirsin. ✨`}
+        className="sticky top-14 z-30 h-10 w-full rounded border border-gray-400 px-4 py-2 text-xs  outline-none placeholder:text-xs dark:border-blue-200  md:text-sm md:placeholder:text-sm"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+    </>
   );
 }
